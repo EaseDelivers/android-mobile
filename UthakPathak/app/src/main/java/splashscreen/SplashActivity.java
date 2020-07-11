27ddbc9a87b2e00ea9ef3ff483d9acf863ebdb62
ParimalKesan <parimalkesan3@gmail.com>
2020-07-12 01:16:33 +0530

    added SplashActivity

--- UthakPathak/app/src/main/java/splashscreen/SplashActivity.java
+++ UthakPathak/app/src/main/java/splashscreen/SplashActivity.java
@@ -1,10 +1,16 @@
 package splashscreen;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 import com.android.uthakpathak.R;
+
+import Authentication.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +18,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //hide actionbar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //create new handler for SplashActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //start the LoginActivity class for user to login
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        }, 2500);//open the new Activity after a delay of 2.5s
     }
 }
