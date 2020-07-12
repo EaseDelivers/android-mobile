c30ddf9141dd8664c28ada99efa026430b3a82c4
ParimalKesan <parimalkesan3@gmail.com>
2020-07-12 12:55:17 +0530

    added animation files in anim directory and added animation code in SplashActivity

--- UthakPathak/app/src/main/java/splashscreen/SplashActivity.java
+++ UthakPathak/app/src/main/java/splashscreen/SplashActivity.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
 import com.android.uthakpathak.R;
@@ -13,11 +16,20 @@ import com.android.uthakpathak.R;
 import Authentication.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    //variable references for animation of appname and applogo
+    Animation anim_translate_lefttoright;
+    Animation anim_translate_righttoleft;
+    //references to applogo and appname
+    ImageView imageview_applogo;
+    ImageView imageview_appname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //method to load animations for appname and applogo
+        loadAnimations();
 
         //hide actionbar
         ActionBar actionBar = getSupportActionBar();
@@ -31,5 +43,19 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             }
         }, 2500);//open the new Activity after a delay of 2.5s
+    }
+
+    //method to load animations for appname and applogo
+    private void loadAnimations()
+    {
+        //get references to applogo and appname
+        imageview_applogo=(ImageView)findViewById(R.id.splashscreen_applogo);
+        imageview_appname=(ImageView)findViewById(R.id.splashscreen_appnname);
+
+        //load animations for applogo and appname and start animations
+        anim_translate_lefttoright=AnimationUtils.loadAnimation(this,R.anim.translate_lefttoright);
+        anim_translate_righttoleft=AnimationUtils.loadAnimation(this,R.anim.translate_righttoleft);
+        imageview_applogo.startAnimation(anim_translate_lefttoright);
+        imageview_appname.startAnimation(anim_translate_righttoleft);
     }
 }
