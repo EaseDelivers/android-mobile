f849c642243c3277814ebf8299ba79cb15a22d54
Divyanshu Gupta <44648798+divyanshuguptaososofficial@users.noreply.github.com>
2020-07-15 15:51:03 +0530

    Google Signup feature Added

--- UthakPathak/app/src/main/java/com/android/uthakpathak/Authentication/SignUpScreen.java
+++ UthakPathak/app/src/main/java/com/android/uthakpathak/Authentication/SignUpScreen.java
@@ -2,27 +2,51 @@ package com.android.uthakpathak.Authentication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.android.uthakpathak.R;
 import com.android.uthakpathak.signup.SignUpActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.hbb20.CountryCodePicker;
 
 
 public class SignUpScreen extends AppCompatActivity {
+    private static final int RC_SIGN_IN =101 ;
     //references to login and register button
-    Button googlebutton;
+    Button GoogleSignup;
     Button emailbutton;
     //references to otpbutton,editextphoneno and ccp
     EditText edittext_phoneno;
     Button button_sendotp;
     CountryCodePicker countrycode;
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +56,27 @@ public class SignUpScreen extends AppCompatActivity {
         //method for phone authentication
         phoneAuthentication();
 
-        Button GoogleSignup= findViewById(R.id.googleButton);
         //hide actionbar
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         //get references to email and google button
-        googlebutton = (Button) findViewById(R.id.googleButton);
+        GoogleSignup = (Button) findViewById(R.id.googleButton);
         emailbutton = (Button) findViewById(R.id.emailButton);
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient   = GoogleSignIn.getClient(this, gso);
+
         //set onclick listener on googlebutton
-        googlebutton.setOnClickListener(new View.OnClickListener() {
+        GoogleSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                startActivity(new Intent(SignUpScreen.this, LoginActivity.class));
+                signIn();
 
             }
         });
@@ -91,6 +122,64 @@ public class SignUpScreen extends AppCompatActivity {
             }
         });
     }
+
+
+
+//google authentication
+        private void signIn() {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+            if (requestCode == RC_SIGN_IN) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                    firebaseAuthWithGoogle(account.getIdToken());
+                } catch (ApiException e) {
+                    // Google Sign In failed, update UI appropriately
+           // ...
+                    Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+
+        private void firebaseAuthWithGoogle(String idToken) {
+            AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+            mAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(SignUpScreen.this,user.getEmail()+user.getDisplayName(),Toast.LENGTH_LONG).show();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(SignUpScreen.this, task.getException().toString(),Toast.LENGTH_LONG).show();
+                                updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+        }
+
+    private void updateUI(FirebaseUser user) {
+        Intent intent= new Intent(SignUpScreen.this,LoginActivity.class);
+        startActivity(intent);
+    }
+
 
     //prevent from going to previous activity
     @Override
